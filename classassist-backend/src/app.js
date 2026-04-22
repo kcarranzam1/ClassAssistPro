7c01aff3cca47787e7abe87877ff1c3d7a2b5c70
kcarranzam1 <kcarranzam1@miumg.edu.gt>
2026-04-22 17:33:08 -0600

    Actualizar CORS para Netlify

--- classassist-backend/src/app.js
+++ classassist-backend/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
   origin: [
     "http://localhost:5173",
-    "https://TU-FRONTEND.netlify.app"
+    "https://classassist-frontend.netlify.app"
   ]
 app.use(express.json());
 app.use("/selfies", express.static("selfies"));
